08807ec7dc174d07d86396e66d7fc43445392bfa
Henry Kim <flashSeok@gmail.com>
2022-07-30 19:47:15 +0900

    텍스트 필드 관련 메서드  추가.(readme 참고)

--- TextFieldPractice/ViewController.swift
+++ TextFieldPractice/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 // 1) ⭐️ViewController 내에서 textField를 사용하려면 UITextFieldDelegeate 프로토콜을 채택해야 한다.⭐️
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
@@ -19,7 +19,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.delegate = self
         
         setUp()
- 
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
     }
     
     // UITextFieldDelegate 프로토콜을 채택했기 때문에 아래 선택적인 메소드를 실행할 수 있음.(선택사항) text치다보면 메서드 리스트들이 뜬다.
@@ -47,7 +50,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         print(#function)
         print(string)
-        return true // false로 설정하면 키보드를 눌러도 입력이 안됨.
+        
+        // 숫자입력 불가.
+        if Int(string) != nil {
+            return false
+        } else {
+            // 글자수 10글자로 제한.
+            guard let text = textField.text else { return true }
+            let newLength = text.count + string.count - range.length
+            
+            return newLength <= 10
+        }
     }
     
     // 텍스트필드의 엔터키가 눌러지면 다음 동작을 허락할 것인지에 대한 여부
@@ -64,7 +77,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // 텍스트필드의 입력이 끝났을 때 호출(시점)
     func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
         print("끝났다")
+        textField.text = ""
     }
     
     
@@ -77,13 +92,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.borderStyle = .roundedRect
         textField.clearButtonMode = .always
         textField.returnKeyType = .next
+        
+        // 앱 화면에 들어오자마자 키보드 활성화
+        // First응답 객체를 지정해줌.(화면에서 일단 먼저 반응할 녀석)
+        // textField가 First응답 객체가 되면 키보드가 올라옴.
+        // 한마디로 유저한테 먼저 반을할 것을 포커스 해줌.
+        textField.becomeFirstResponder()
     }
-
     
     @IBAction func doneButtonTapped(_ sender: UIButton) {
         
+        // 키보드가 내려감.
+        textField.resignFirstResponder()
         
     }
+    
+    // 다른 곳 누르면 키보드 내려감.
+    @objc func dismissKeyboard() {
+        print(#function)
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
     
 }
 
